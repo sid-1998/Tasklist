c82edda5726101a4a44cdfba4a751754cd7aab04
Siddharth Pahuja <sid.pahuja2008@gmail.com>
2020-05-12 14:27:03 +0530

    adding task feature
    Signed-off-by: Siddharth Pahuja <sid.pahuja2008@gmail.com>

---
+++ app.js
@@ -0,0 +1,28 @@
+const form = document.querySelector('#task-form');
+const taskInput = document.querySelector('#task');
+const filter = document.querySelector('#filter');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+
+form.addEventListener('submit', addTask);
+
+function addTask(e){
+    if(taskInput.value === ''){
+        alert('Add a task');
+    }
+    // create a li element
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    const textNode = document.createTextNode(taskInput.value);
+    li.appendChild(textNode);
+    
+    //delete icon
+    const link = document.createElement('a');
+    link.className = 'detele-item secondary-content';//secondary-content is a materialize class added to make the icon appear on the left side.
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+
+    li.appendChild(link);
+    taskList.appendChild(li);
+    taskInput.value = '';
+    e.preventDefault();
+}
